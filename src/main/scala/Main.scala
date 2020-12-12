8c536f97f5e39a3968951a488c2f364fb1c66578
matkob <nologin90@gmail.com>
2020-12-12 21:44:46 +0100

    error mapper with stdout

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,10 +9,20 @@ import scala.util.Try
 
 object Main extends App {
 
+  val errorMapper: Throwable => Int = {
+    t =>
+      println(s"error ${t.getClass.getSimpleName}")
+      1
+  }
+
+  val resultMapper: Any => Int = _ => 0
+
   val result = for {
     input <- Try(new File(args(0))).toEither
     minSupport <- Try(MinSupport(args(1).toInt)).toEither
     txs <- Reader.transactions(input)
     associationRules <- Eclat.associationRules(txs, minSupport)
   } yield associationRules
+
+  sys.exit(result.fold(errorMapper, resultMapper))
 }
